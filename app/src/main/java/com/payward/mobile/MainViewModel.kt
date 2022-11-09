af163915681f3ebfdbb40156f6fc12058e8d57f8
Natalie Forbes <natalie@sevenhillstechnology.com>
2022-11-09 10:21:32 -0500

    firestore

--- app/src/main/java/com/payward/mobile/MainViewModel.kt
+++ app/src/main/java/com/payward/mobile/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.payward.mobile
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.payward.mobile.dto.Request
 import com.payward.mobile.service.RequestService
 
@@ -9,7 +12,50 @@ class MainViewModel : ViewModel() {
     var requests: MutableLiveData<ArrayList<Request>> = MutableLiveData<ArrayList<Request>>()
     var requestService: RequestService = RequestService()
 
-    fun fetchRequests(requestId: Int) {
+    private lateinit var firestore : FirebaseFirestore
+
+    init {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+        listenForRequests()
+    }
+
+    private fun listenForRequests() {
+        firestore.collection("requests").addSnapshotListener {
+                snapshot, e ->
+            if (e != null) {
+                Log.w("Listen failed", e)
+                return@addSnapshotListener
+            }
+            snapshot?.let {
+                val allRequests = ArrayList<Request>()
+                val documents = snapshot.documents
+                documents.forEach {
+                    val request = it.toObject(Request::class.java)
+                    request?.let {
+                        allRequests.add(it)
+                    }
+                }
+                requests.value = allRequests
+            }
+        }
+    }
+
+    fun fetchRequests(requestId: String) {
         requests = requestService.fetchRequests(requestId)
+    }
+
+    fun save(request: Request) {
+        val document = if (request.requestId == null || request.requestId.isEmpty()) {
+            //add
+            firestore.collection("requests").document()
+        } else {
+            //update
+            firestore.collection("requests").document(request.requestId)
+        }
+        request.requestId = document.id
+        val handle = document.set(request)
+        handle.addOnSuccessListener { Log.d("Firebase", "Document Saved") }
+        handle.addOnFailureListener { Log.e("Firebase", "Save failed $it ")}
     }
 }
