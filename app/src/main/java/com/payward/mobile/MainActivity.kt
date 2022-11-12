fcdd2199bb8ba93bfb77126ee430098152e42859
surapasi <surapasi@mail.uc.edu>
2022-11-12 01:05:18 -0500

    implementations

--- app/src/main/java/com/payward/mobile/MainActivity.kt
+++ app/src/main/java/com/payward/mobile/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.payward.mobile
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
