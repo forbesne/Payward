d87cdb26dc0a1c1d5f6d25b3ce9846a099ef1bb3
surapasi <surapasi@mail.uc.edu>
2022-11-12 00:40:43 -0500

    minor layout changes and emulator works fine now

--- app/src/main/java/com/payward/mobile/MainActivity.kt
+++ app/src/main/java/com/payward/mobile/MainActivity.kt
@@ -1,41 +1,14 @@
 package com.payward.mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.payward.mobile.ui.theme.PaywardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
-        setContent {
-            PaywardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PaywardTheme {
-        Greeting("Android")
     }
 }
