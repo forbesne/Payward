d8458d3b2e868bfb5c5c00aa37290144fc0e31a2
Natalie Forbes <natalie@sevenhillstechnology.com>
2022-11-12 16:56:31 -0500

    updated firebase service and connected with mvm

--- app/src/main/java/com/payward/mobile/dto/Request.kt
+++ app/src/main/java/com/payward/mobile/dto/Request.kt
@@ -1,4 +1,4 @@
 package com.payward.mobile.dto
 
-class Request(var requestId : String, var userId : Int, var city : String, var issueType : String, var text : String) {
+class Request(var requestId : String = "", var userId : String = "", var city : String = "", var issueType : String = "", var text : String = "") {
 }
