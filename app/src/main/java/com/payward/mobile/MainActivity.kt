c312fcf9b974111ee0fb7c0465699afd91f8089c
surapasi <surapasi@mail.uc.edu>
2022-11-15 18:20:29 -0500

    help request and button

--- app/src/main/java/com/payward/mobile/MainActivity.kt
+++ app/src/main/java/com/payward/mobile/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.payward.mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 
@@ -11,6 +13,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
+
+        val helpRequestButton = findViewById<Button>(R.id.helpRequestBtn)
+        helpRequestButton.setOnClickListener {
+          this.setContentView(R.layout.activity_help_request)
+        }
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.initializeFirebase()
